ed944a7643c76757b6ef3fc871d740045f327f60
Anartz Mugika Ledo <mugan86@gmail.com>
2022-01-09 20:25:35 +0100

    Add new test in ProductItem to complete component

--- src/lib/tests/components/ProductItem.test.js
+++ src/lib/tests/components/ProductItem.test.js
@@ -2,12 +2,15 @@ import React from "react";
 import "@testing-library/jest-dom";
 import { shallow } from "enzyme";
 import { ProductItem } from "./../../components/ProductItem";
+import { CONSTANTS } from "../../constants";
 describe("Pruebas en ProductItem", () => {
   let wrapper;
   const text = `<i className="fas fa-info-circle"></i>`;
   const handleAction = jest.fn();
   const showDetails = jest.fn();
   const addCart = jest.fn();
+  const selectMoney = CONSTANTS.CURRENCY_LIST.EURO;
+  const showDesc = true;
   const item = {
     id: "1222",
     price: 23,
@@ -28,8 +31,9 @@ describe("Pruebas en ProductItem", () => {
       <ProductItem
         item={item}
         addCart={addCart}
-        showDesc={true}
+        showDesc={showDesc}
         showDetails={showDetails}
+        selectMoney={selectMoney}
       />
     );
   });
@@ -56,4 +60,44 @@ describe("Pruebas en ProductItem", () => {
     // expect(buttonComponent.at(1).prop("handleAction")).toBeDefined(addCart);
   });
 
+  test("Comprobar elementos con los datos si son correctos o no", () => {
+    const productNameContent = wrapper.find(".truncate-one-line").text();
+    expect(productNameContent).toBe(item.product.name);
+
+    const productDescriptionElement = wrapper.find(".description");
+    showDesc
+      ? expect(productDescriptionElement.text()).toBe(item.platform.name)
+      : expect(productDescriptionElement).toMatchObject({});
+
+    const priceContent = wrapper.find(".top-div span").text();
+    expect(priceContent).toBe(`€ ${item.price}`);
+  });
+
+  test("Comprobar que seleccionamos correctamente las monedas para mostrar", () => {
+    const currenciesValues = Object.values(CONSTANTS.CURRENCY_LIST);
+    Object.keys(CONSTANTS.CURRENCY_LIST).map((__, index) => {
+      checkIfMoneySymbolIsCorrect(
+        currenciesValues[index],
+        CONSTANTS.CURRENCIES_SYMBOL[currenciesValues[index]],
+        item
+      );
+    });
+  });
 });
+
+const checkIfMoneySymbolIsCorrect = (selectMoney, expectedSymbol, item) => {
+  console.log(expectedSymbol, item, selectMoney);
+  const product = shallow(
+    <ProductItem
+      item={item}
+      addCart={jest.fn()}
+      showDesc={false}
+      showDetails={jest.fn()}
+      selectMoney={selectMoney}
+    />
+  );
+  const priceContent = product.find(".top-div span").text();
+  expect(priceContent).toBe(`${expectedSymbol} ${item.price}`);
+
+  expect(product).toMatchSnapshot();
+};

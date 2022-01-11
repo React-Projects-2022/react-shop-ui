93304ae617da04d95b5221666c6fe20828b182cf
Anartz Mugika Ledo <mugan86@gmail.com>
2022-01-11 20:25:19 +0100

    Add Carousel with resolve issue

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,13 @@
 import { QuantitySelector } from "./components/QuantitySelector";
 import { CONSTANTS } from "./constants";
-import { QuantitySelector } from "./components/QuantitySelector";
-import { CONSTANTS } from "./constants";
 import { ProducItem } from "./components/ProductItem";
 import { ProductItemGroup } from "./components/ProductItemGroup";
 import { Rating } from "./components/Rating";
 import { Button } from "./components/Button";
+import { Carousel } from "./components/Carousel";
 export {
     Button,
+    Carousel,
     ProducItem,
     ProductItemGroup,
     Rating,

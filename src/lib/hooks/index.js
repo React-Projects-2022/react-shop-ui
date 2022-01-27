ac3b6ccdaf5977714d09a57cdb8eb96ad88338b4
Anartz Mugika Ledo <mugan86@gmail.com>
2022-01-27 19:50:59 +0100

    Work with LTS version

--- src/lib/hooks/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { useCounter } from "./useCounter";
-import { useCart } from "./useCart";
-
-export { useCounter, useCart };
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,13 @@
 import React from "react";
-
+import { QuantitySelector } from "./components/QuantitySelector";
 import { CONSTANTS } from "./constants";
+import { ProductItem } from "./components/ProductItem";
+import { ProductItemGroup } from "./components/ProductItemGroup";
+import { Rating } from "./components/Rating";
+import { Button } from "./components/Button";
+import { Carousel } from "./components/Carousel";
+import { Cart } from "./components/Cart";
 import { useCart } from "./hooks/useCart";
-import {
-  Button,
-  Carousel,
-  Cart,
-  ProductItem,
-  ProductItemGroup,
-  Rating,
-  QuantitySelector,
-} from "./components";
 export {
   Button,
   Carousel,
